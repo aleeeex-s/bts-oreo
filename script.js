3ba957b2f1f1fbf3840cafe7c657f0dc429541bf
alex <alexsolis1807@gmail.com>
2026-05-27 18:11:55 -0300

    Refactor drag functionality and add updatePack method

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const inside = document.querySelector(".inside");
 let isDragging = false;
 
 let startX = 0;
-let currentX = 0;
+let currentMove = 0;
+
+let isOpen = false;
 
 rightPack.addEventListener("mousedown", startDrag);
 window.addEventListener("mousemove", drag);
@@ -15,6 +17,8 @@ window.addEventListener("touchmove", drag);
 window.addEventListener("touchend", stopDrag);
 
 function startDrag(e){
+
+  if(isOpen) return;
 
   isDragging = true;
 
@@ -27,28 +31,44 @@ function drag(e){
 
   if(!isDragging) return;
 
-  currentX = getX(e);
+  let currentX = getX(e);
 
-  let move = currentX - startX;
+  currentMove = currentX - startX;
 
-  if(move < 0) move = 0;
-  if(move > 180) move = 180;
+  if(currentMove < 0) currentMove = 0;
+  if(currentMove > 180) currentMove = 180;
 
-  rightPack.style.transform =
-    `translateX(${move}px) rotate(${move * 0.05}deg)`;
-
-  inside.style.opacity = move / 100;
+  updatePack(currentMove);
 }
 
 function stopDrag(){
+
+  if(!isDragging) return;
 
   isDragging = false;
 
   rightPack.style.transition = "0.4s ease";
 
-  rightPack.style.transform = `translateX(0px)`;
+  // SI abrió suficiente
+  if(currentMove > 90){
 
-  inside.style.opacity = 0;
+    currentMove = 180;
+    isOpen = true;
+
+  }else{
+
+    currentMove = 0;
+  }
+
+  updatePack(currentMove);
+}
+
+function updatePack(move){
+
+  rightPack.style.transform =
+    `translateX(${move}px) rotate(${move * 0.05}deg)`;
+
+  inside.style.opacity = move / 100;
 }
 
 function getX(e){
